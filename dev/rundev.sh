efb45d1a5b75fe5c8b004192ffd10a7d9eae2736
liangliang <liangliangyy@gmail.com>
2018-04-25 22:41:36 +0800

    update

--- dev/rundev.sh
+++ dev/rundev.sh
@@ -1,7 +1,15 @@
 basedir=`pwd`
-docker rm mysqldev
+cd ../../service_configs/mysql/conf.d/
+configdir=`pwd`
+cd $basedir
+
+docker rm mysqldev memcachedev -f
 #mysql
-docker run --name mysqldev -p 3306:3306  -v $basedir/mysql:/var/lib/mysql -e  MYSQL_ROOT_PASSWORD=root -d mysql
+docker run --name mysqldev -p 3306:3306 \
+  -v $basedir/mysql:/var/lib/mysql  \
+  -v $configdir:/etc/mysql/conf.d/ \
+  -e  MYSQL_ROOT_PASSWORD=root \
+   -d mysql
 #memcached
 docker run --name memcachedev -d memcached memcached -m 64 -p 11211:11211
 
